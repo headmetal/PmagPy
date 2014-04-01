0192809e3d408c6a62be78a9bce8c0ac63440b8a
Lori J <mintblue87@gmail.com>
2014-03-31 18:15:57 -0700

    testing exactly which error/exception scenarios cause a build to fail

--- tester.sh
+++ tester.sh
@@ -5,7 +5,13 @@ pwd
 echo 'running tester bash script'
 python Command_line_Tests/fake_test.py
 echo 'finished running python program'
+echo 'try igrf'
+python Command_line_Tests/Rename_me.py -r igrf
 echo 'try lowrie'
 python Command_line_Tests/Rename_me.py -r lowrie
-python Command_line_Tests/Rename_me.py -r igrf
 echo 'finish lowrie'
+
+#if [ "foo" == "foo" ];
+#then
+#    exit 1
+#fi
