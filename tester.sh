06ed2e79ad5fadce03f213ab099fc52083bcc8ba
Lori J <mintblue87@gmail.com>
2014-03-31 18:00:02 -0700

    try bash script with failure

--- tester.sh
+++ tester.sh
@@ -7,4 +7,5 @@ python Command_line_Tests/fake_test.py
 echo 'finished running python program'
 echo 'try lowrie'
 python Command_line_Tests/Rename_me.py -r lowrie
+python Command_line_Tests/Rename_me.py -r igrf
 echo 'finish lowrie'
