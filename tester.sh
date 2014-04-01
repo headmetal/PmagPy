27d134f09e09d85231f71fca105d9055ff0ac233
Lori J <mintblue87@gmail.com>
2014-04-01 12:45:02 -0700

    experiment with bash script codes

--- tester.sh
+++ tester.sh
@@ -7,11 +7,24 @@ python Command_line_Tests/fake_test.py
 echo 'finished running python program'
 echo 'try igrf'
 python Command_line_Tests/Rename_me.py -r igrf
+exit_code1=$?
+echo 'igrf exit code' $exit_code1
 echo 'try lowrie'
 python Command_line_Tests/Rename_me.py -r lowrie
+exit_code2=$?
+echo 'lowrie exit code' $exit_code2
 echo 'finish lowrie'
 
-#if [ "foo" == "foo" ];
-#then
-#    exit 1
-#fi
+code_total=$(($exit_code1+$exit_code2))
+
+
+if [ $code_total != 0 ];
+then
+    echo 'exiting with 1'
+    exit 1
+else
+    echo 'exiting with 0'
+    exit 0
+fi
+
+
