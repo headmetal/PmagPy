bbd4428b942b0a6bd6035cd9e5d5ecb0c45732be
Lori J <mintblue87@gmail.com>
2014-03-31 17:55:51 -0700

    try bash script again, without spelling errors

--- tester.sh
+++ tester.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+echo 'pwd'
+pwd
 echo 'running tester bash script'
-python Command_line_tests/fake_test.py
+python Command_line_Tests/fake_test.py
 echo 'finished running python program'
 echo 'try lowrie'
-python Command_line_tests/Rename_me.py -r lowrie
+python Command_line_Tests/Rename_me.py -r lowrie
 echo 'finish lowrie'
